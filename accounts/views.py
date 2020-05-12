9061f50c12fe783b6555f4a76df96eb2143429b4
YNNJN <dbswls1014@naver.com>
2020-05-12 09:55:09 +0000

    Authentication & Follow

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm
+from .models import User
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+
+def profile(request, id):
+    user_info = get_object_or_404(User, id=id)
+    context = {
+        'user_info': user_info
+    }
+    return render(request, 'accounts/profile.html', context)
+
+def follow(request, id):
+    you = get_object_or_404(User, id=id)
+    me = request.user
+    if you == me:
+        return redirect('account:profile', id)
+    if me in you.followers.all():
+        you.followers.remove(me)
+    else:
+        you.followers.add(me)
+    return redirect('accounts:profile', id)
